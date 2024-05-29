0c1402644f3cd5410abfe8e57069ed1f2b980cd8
neo-30003 <dr.arrum@gmail.com>
2024-05-29 23:08:01 +0300

    Выполнил второе усложнённое задание

---
+++ script.js
@@ -0,0 +1,13 @@
+let num = 266219,
+  sum,
+  degree;
+
+alert("Внимание! Внимание!");
+console.log("Приветствую Вас!)");
+sum = 2 * 6 * 6 * 2 * 1 * 9;
+console.log(sum);
+
+degree = sum ** 3;
+console.log(degree);
+
+console.log(String(degree).slice(0, 2));
